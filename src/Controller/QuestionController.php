a77e127b33bf4b0ee13ef85e55d49f0a32ae8fe9
daer4n <fuzzebf@gmail.com>
2021-10-13 16:22:13 +0200

    integration twig

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,23 +4,26 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController {
+class QuestionController extends AbstractController{
 	/** 
 		* @Route("/", name="app_question_home") 
 	*/ 
 	public function homepage () {
-		return new Response("<h1>Hello World</h1>");
+		return $this->render('base.html.twig');
+		// return new Response("<h1>Hello World</h1>");
 	}
 
 	/** 
 	* @Route("/questions/{slug}", name="app_question_show") 
 	*/ 
-	public function show(string $slug)
+	public function show(string $slug) : Response
 	{
 		$slug = ucfirst(str_replace("-", " ", $slug));
-		return new Response("<h1>$slug ?</h1>");
+		return $this->render( 'questions/show.html.twig', ['slug' => $slug] );
+		// return new Response("<h1>$slug ?</h1>");
 	}
 }
