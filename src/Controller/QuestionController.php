efacecd80991b09bac525efe7261fbcda51d2188
daer4n <fuzzebf@gmail.com>
2021-11-05 09:31:35 +0100

    Ajout et config de docker

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -7,16 +7,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\DTO\Question;
-use App\DTO\Author;
-use Twig\Environment;
+use App\Service\MarkdownHelper;
+
+// use App\DTO\Question;
+// use App\DTO\Author;
+// use Twig\Environment;
 
 class QuestionController extends AbstractController{
 
 	/** 
 	* @Route("/", name="app_question_home") 
 	*/ 
-	public function homepage (/* Environment $twig */) {
+	public function homepage (/* Environment $twig */) : Response {
 
 		// return new Response( $twig->render('question/homepage.html.twig') );
 		return $this->render('question/homepage.html.twig');
@@ -25,19 +27,20 @@ class QuestionController extends AbstractController{
 	/** 
 	* @Route("/question/{slug}", name="app_question_show") 
 	*/ 
-	public function show($slug) : Response
+	public function show($slug, MarkdownHelper $helper) : Response
 	{
-		// $slug = ucfirst(str_replace("-", " ", $slug));
-		// return $this->render( 'question/show.html.twig', [
-		// 	'question' => [
-		// 		'slug' => $slug
-		// 	]
-		// ]);
-		$test = new Question( new Author('Alexis', 'Couturas'), $slug);
+		$content = "Je suis tombé **sans faire exprès** dans un trou noir, pouvez vous m'indiquer comment sortir de là ?";
 
+		$question = [
+			'slug' => ucfirst(str_replace("-", " ", $slug)),
+			'content' => $content
+		];
+
+		// $question = new Question( new Author('Alexis', 'Couturas'), $slug);
 
 		return $this->render(
-			'question/show.html.twig'
+			'question/show.html.twig',
+			[ 'question' => $question ]
 		);
 	}
 }
---
+++ src/Service/MarkdownHelper.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+
+final class MarkdownHelper
+{
+	private MarkdownParserInterface $markdown;
+	private CacheInterface $cache;
+
+	public function __construct(MarkdownParserInterface $markdown, CacheInterface $cache){
+		$this->markdown = $markdown;
+		$this->cache = $cache;
+	}
+
+	public function parse(string $source): string
+	{
+
+		$source = $this->cache->get(md5($source), function(ItemInterface $item) use ($source){
+			sleep(2);
+			$item->expiresAfter(20);
+			return $this->markdown->transformMarkdown($source);
+		});
+
+		return $source;
+	}
+}
